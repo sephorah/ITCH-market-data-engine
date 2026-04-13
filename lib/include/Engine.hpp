42970470cc50aa1f3fe3f300ee9bbcc48026b0d9
Sephorah ANIAMBOSSOU <sephorah.aniambossou@epitech.eu>
2026-04-13 04:11:31 -0400

    refactor(engine): itch messages parsing

--- lib/include/Engine.hpp
+++ lib/include/Engine.hpp
@@ -14,13 +14,17 @@
 #include "ItchParser.hpp"
 #include "Printer.hpp"
 
-static constexpr std::size_t BUFFER_SIZE = 64 * 1024;
+constexpr std::size_t BUFFER_SIZE = 64 * 1024;
+constexpr std::size_t LENGTH_FIELD_SIZE = 2;
 
 class Engine
 {
 public:
     explicit Engine(const std::string &configFilename);
     void run();
+    std::size_t processMessages(std::unique_ptr<char[]> &buffer, std::size_t count, std::size_t &unknownCount);
+    std::size_t getMessageLength(std::unique_ptr<char[]> &buffer, std::size_t &baseOffset) const;
+    void processITCHFile(std::ifstream &itchFile);
 
 private:
     std::string _itchFilename;
--- lib/src/Engine.cpp
+++ lib/src/Engine.cpp
@@ -14,55 +14,78 @@ Engine::Engine(const std::string &configFilename)
     }
 }
 
-void Engine::run()
+std::size_t Engine::getMessageLength(std::unique_ptr<char[]> &buffer, std::size_t &baseOffset) const
 {
-    std::ifstream itchFile(_itchFilename, std::ios::binary);
-    auto buffer = std::make_unique<char[]>(BUFFER_SIZE);
+    uint16_t bigEndianLength;
+
+    std::memcpy(&bigEndianLength, buffer.get() + baseOffset, sizeof(bigEndianLength));
+    std::size_t length = __builtin_bswap16(bigEndianLength);
+    return length;
+}
+
+std::size_t Engine::processMessages(std::unique_ptr<char[]> &buffer, std::size_t count, std::size_t &unknownCount)
+{
+    std::size_t leftoversBytes = 0;
+    std::size_t baseOffset = 0;
     Printer printer;
 
-    if (!itchFile)
+    while (true)
     {
-        throw std::runtime_error("Failed to open " + _itchFilename);
+        if (baseOffset + LENGTH_FIELD_SIZE > count)
+        {
+            leftoversBytes = count - baseOffset;
+            std::memmove(buffer.get(), buffer.get() + baseOffset, leftoversBytes);
+            return leftoversBytes;
+        }
+        std::size_t length = getMessageLength(buffer, baseOffset);
+        baseOffset += LENGTH_FIELD_SIZE;
+        if (baseOffset + length > count)
+        {
+            leftoversBytes = count - baseOffset + LENGTH_FIELD_SIZE;
+            std::memmove(buffer.get(), buffer.get() + baseOffset - LENGTH_FIELD_SIZE, leftoversBytes);
+            return leftoversBytes;
+        }
+        else
+        {
+            Itch::ParseResult result = Itch::parse(buffer.get() + baseOffset, length, printer);
+            baseOffset += result.bytes;
+            if (result.status == Itch::ParseStatus::UnknownMessageType)
+                unknownCount++;
+        }
     }
+}
+
+void Engine::processITCHFile(std::ifstream &itchFile)
+{
+    std::unique_ptr<char[]> buffer = std::make_unique<char[]>(BUFFER_SIZE);
     std::size_t leftoversBytes = 0;
     std::size_t bytesRead = 0;
     std::size_t count = 0;
     std::size_t baseOffset = 0;
     std::size_t unknownCount = 0;
+
     while (true)
     {
         itchFile.read(buffer.get() + leftoversBytes, BUFFER_SIZE - leftoversBytes);
         bytesRead = itchFile.gcount();
         if (bytesRead == 0)
+        {
             break;
+        }
         count = bytesRead + leftoversBytes;
         baseOffset = 0;
-        while (true)
-        {
-            if (baseOffset + 2 > count)
-            {
-                leftoversBytes = count - baseOffset;
-                std::memmove(buffer.get(), buffer.get() + baseOffset, leftoversBytes);
-                break;
-            }
-            uint16_t bigEndianLength;
-            std::memcpy(&bigEndianLength, buffer.get() + baseOffset, sizeof(bigEndianLength));
-            std::size_t length = __builtin_bswap16(bigEndianLength);
-            baseOffset += 2;
-            if (baseOffset + length > count)
-            {
-                leftoversBytes = count - baseOffset + 2;
-                std::memmove(buffer.get(), buffer.get() + baseOffset - 2, leftoversBytes);
-                break;
-            }
-            else
-            {
-                Itch::ParseResult result = Itch::parse(buffer.get() + baseOffset, length, printer);
-                baseOffset += result.bytes;
-                if (result.status == Itch::ParseStatus::UnknownMessageType)
-                    unknownCount++;
-            }
-        }
+        leftoversBytes = processMessages(buffer, count, unknownCount);
     }
     std::cout << "Unknown messages count: " << unknownCount << std::endl;
+}
+
+void Engine::run()
+{
+    std::ifstream itchFile(_itchFilename, std::ios::binary);
+
+    if (!itchFile)
+    {
+        throw std::runtime_error("Failed to open " + _itchFilename);
+    }
+    processITCHFile(itchFile);
 }
